8139f1aad4ca5251133e4a9f8ace7b655c286be8
PedroGgurski <139177045+PedroGgurski@users.noreply.github.com>
2023-10-04 22:54:36 -0300

    Update TipoVariaveis.java

---
+++ Tipos-variaveis/src/TipoVariaveis.java
@@ -0,0 +1,11 @@
+public class TipoVariaveis {
+    /**
+     * @param args
+     * @throws Exception
+     */
+    public static void main(String[] args) throws Exception {
+       int numero = 5;
+       numero = 10;
+       
+    }
+}
